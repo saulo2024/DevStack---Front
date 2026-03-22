bb826b188bd7d1a4debefd62b5a2a61777e539a9
estevesdev <sauloestevesdealmeida27@gmail.com>
2026-03-21 20:02:08 -0400

    Connecting app.tsx to main.tsx

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,10 +1,11 @@
-function App() {
-  return (
-    <div>
-      <h1>DevStack: Tijolo por Tijolo</h1>
-      <p>A interface está nascendo!</p>
-    </div>
-  )
-}
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App"; // Importa o seu componente principal
+import './index.css'; // Importa o Tailwind/CSS
 
-export default App
+// Essa é a linha que 'liga' o React ao HTML
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
